e5444b19dee542080c6d864be17ecf4129c1a175
Dmitry Khodyrev <dooommm@mail.ru>
2023-06-20 20:01:41 +0800

    fix mongo

--- shared/libs/mongodb.ts
+++ shared/libs/mongodb.ts
@@ -1,10 +1,10 @@
 import { MongoClient } from 'mongodb';
 
-if (!process.env.MONGODB_URI) {
+if (!process.env.DB_URL) {
   throw new Error('Please add your Mongo URI to .env.local');
 }
 
-const uri: string = process.env.MONGODB_URI;
+const uri: string = process.env.DB_URL;
 const options = {};
 
 let client: MongoClient;
