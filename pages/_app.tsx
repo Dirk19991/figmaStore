9de35a8f2670bcbe379b254f7ce98fccaa504933
Dmitry Khodyrev <dooommm@mail.ru>
2023-07-21 20:35:13 +0800

    fix fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,25 +3,35 @@ import Layout from 'layout/Layout';
 import CartContextProvider from '../context/CartContextProvider';
 import CountryContextProvider from '../context/CountryContextProvider';
 import 'shared/styles/global.css';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps, ...appProps }: AppProps) {
   if ([`/checkout`].includes(appProps.router.pathname)) {
     return (
-      <CartContextProvider>
-        <CountryContextProvider>
-          <Component {...pageProps} />;
-        </CountryContextProvider>
-      </CartContextProvider>
+      <div lang='en' className={inter.className}>
+        <CartContextProvider>
+          <CountryContextProvider>
+            <Component {...pageProps} />;
+          </CountryContextProvider>
+        </CartContextProvider>
+      </div>
     );
   }
 
   return (
-    <CartContextProvider>
-      <CountryContextProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </CountryContextProvider>
-    </CartContextProvider>
+    <div lang='en' className={inter.className}>
+      <CartContextProvider>
+        <CountryContextProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </CountryContextProvider>
+      </CartContextProvider>
+    </div>
   );
 }
